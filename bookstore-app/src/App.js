833edfd7bff3cb7f5f1e34c5e27486fe8d6bbe5e
AishwaryaAnaspure <anaspureaish123@gmail.com>
2025-01-30 17:44:06 +0530

    App.js Upadated with relevent code

--- bookstore-app/src/App.js
+++ bookstore-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter  as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { BookProvider } from "./context/BookContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Books from "./pages/Books";
+import BookDetail from "./pages/BookDetail";
+import PrivateRoute from "./pages/PrivateRoute";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <BookProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/books" element={<PrivateRoute><Books /></PrivateRoute>} />
+            <Route path="/books/:id" element={<PrivateRoute><BookDetail /></PrivateRoute>} />
+          </Routes>
+        </Router>
+      </BookProvider>
+    </AuthProvider>
   );
-}
 
+};
 export default App;
